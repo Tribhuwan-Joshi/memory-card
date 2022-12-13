3e3218c6096799cc03e968732d71c2c137e6a92b
Tribhuwan <mathincosmos@gmail.com>
2022-12-13 14:18:53 +0530

    add card layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import git from "../assets/git.png";
 
 export default function Footer() {
   return (
-    <footer className=" border-t-4  boreder-st border-t-slate-600 h-[5%] flex justify-center items-center text-lg font-[monospace]">
+    <footer className=" h-[5%] flex justify-center items-center text-lg font-[monospace]">
       Crafted by Tjsm{" "}
       <a
         target="_blank"
